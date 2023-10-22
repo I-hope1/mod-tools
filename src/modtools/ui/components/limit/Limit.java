09881efdb156a8e3e48bc8b4465bd63bdc4fc345
I-hoep1 <78016895+I-hope1@users.noreply.github.com>
2023-10-22 09:56:50 +0800

    < Updated >
    - 修复Limit不正确隐藏元素的bug

--- src/modtools/ui/components/limit/Limit.java
+++ src/modtools/ui/components/limit/Limit.java
@@ -25,34 +25,27 @@ public interface Limit {
 	Vec2 v1 = new Vec2(), v2 = new Vec2();
 
 	static boolean isVisible(Element actor) {
-		Element elem = actor.parent;
-		while (!(elem instanceof ScrollPane)) {
-			if (elem == null) return false;
-			elem = elem.parent;
+		Element pane = actor.parent;
+		while (!(pane instanceof ScrollPane)) {
+			if (pane == null) return false;
+			pane = pane.parent;
 		}
 
-		absPos(elem, v1);
-		boolean computeIfOverStage = v1.x + elem.getWidth() > Core.graphics.getWidth()
-																 || v1.y + elem.getHeight() < Core.graphics.getHeight()
+		pane.localToStageCoordinates(v1.set(0, 0));
+		boolean computeIfOverStage = v1.x + pane.getWidth() > Core.graphics.getWidth()
+																 || v1.y + pane.getHeight() < Core.graphics.getHeight()
 																 || v1.x < 0 || v1.y < 0;
 
 		/* w, h > 0 */
 		float w = actor.getWidth(), h = actor.getHeight();
 		if (computeIfOverStage) {
-			absPos(elem, v2);
+			actor.localToStageCoordinates(v2.set(0, 0));
 			if (v2.x < -w || v2.y < -h || v2.x > Core.graphics.getWidth() || v2.y > Core.graphics.getHeight()) {
 				return false;
 			}
 		}
-		actor.localToAscendantCoordinates(elem, v1.set(0, 0));
-		return v1.x > -w && v1.y > -h && v1.x < elem.getWidth() && v1.y < elem.getHeight();
+		actor.localToAscendantCoordinates(pane, v1.set(0, 0));
+		return v1.x > -w && v1.y > -h && v1.x < pane.getWidth() && v1.y < pane.getHeight();
 		// return v1.x > -w && v1.y > -h && v1.x < w + elem.getWidth() && v1.y < h + elem.getHeight();
-	}
-	static void absPos(Element elem, Vec2 vec2) {
-		// if (elem.parent instanceof LimitTable l) {
-		// 	elem.localToParentCoordinates(vec2.set(l.absPos));
-		// } else {
-		elem.localToStageCoordinates(vec2.set(0, 0));
-		// }
 	}
 }

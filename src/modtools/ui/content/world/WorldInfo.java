64644afda83a7a4c436444334244deafe3cf73e7
I-hoep1 <78016895+I-hope1@users.noreply.github.com>
2023-12-24 14:54:03 +0800

    ....

--- src/modtools/ui/content/world/WorldInfo.java
+++ src/modtools/ui/content/world/WorldInfo.java
@@ -4,14 +4,22 @@ import arc.graphics.Color;
 import arc.scene.Element;
 import arc.scene.ui.layout.Table;
 import arc.util.Align;
+import mindustry.gen.Building;
+import mindustry.graphics.Pal;
 import mindustry.world.Tile;
 import modtools.ui.IntUI;
+import modtools.utils.SR.SatisfyException;
+import modtools.utils.Tools;
 
 public class WorldInfo {
 	public static void showInfo(Element element, Object o) {
 		IntUI.showSelectTable(element, (p, hide, cont) -> {
-			if (o instanceof Tile) build(p, (Tile) o);
-			else p.add("TODO");
+			try {
+				Tools.Sr(o)
+				 .isInstance(Tile.class, x -> build(p, x))
+				 .isInstance(Building.class, x -> build(p, x))
+				 .isInstance(Object.class, x -> p.add("TODO"));
+			} catch (SatisfyException ignored) {}
 		}, false, Align.center);
 	}
 	public static void build(Table p, Tile tile) {
@@ -26,6 +34,36 @@ public class WorldInfo {
 			p.row().add("ore").color(Color.lightGray);
 			p.image(tile.drop().fullIcon).size(24);
 			p.add(tile.drop().name);
+		}
+	}
+	public static void build(Table p, Building build) {
+		p.left().defaults().left();
+		// build的所有items，build.items
+		if (build.items != null) {
+			p.add("ITEMS").color(Pal.accent).row();
+			p.table(it -> {
+				it.left().defaults().left();
+				build.items.each((item, amount) -> {
+					it.image(item.uiIcon).padRight(6f);
+					it.add(item.localizedName).growX().left().padRight(6f);
+					it.add("" + amount);
+					it.row();
+				});
+			}).row();
+		}
+		// build的所有liquids，build.liquids
+		if (build.liquids != null) {
+			p.add("LIQUIDS").color(Pal.accent).row();
+			p.table(it -> {
+				it.left().defaults().left();
+				it.left().defaults().left();
+				build.liquids.each((liquid, amount) -> {
+					it.image(liquid.uiIcon).padRight(6f);
+					it.add(liquid.localizedName).growX().left().padRight(6f);
+					it.add("" + amount);
+					it.row();
+				});
+			});
 		}
 	}
 }

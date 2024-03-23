e3d78a2baffc749faecd22538cd5405d3da63eef
I-hoep1 <78016895+I-hope1@users.noreply.github.com>
2024-03-23 23:20:52 +0800

    小细节改动

--- src/modtools/utils/draw/InterpImage.java
+++ src/modtools/utils/draw/InterpImage.java
@@ -30,9 +30,10 @@ public class InterpImage extends Element {
 		Draw.alpha(parentAlpha * color.a);
 		selfPointAmount++;
 		if (selfPointAmount > pointAmount + 30) selfPointAmount = 0;
+		/* 左下角 */
 		float x0 = this.x + width / 8f, y0 = this.y + height / 8f,
 		 w = width / 8f * 6f, h = height / 8f * 6f;
-		float lastX = x0 + 0, lastY = y0 + interp.apply(0);
+		float lastX = x0 + 0, lastY = y0 + h * interp.apply(0);
 		Lines.stroke(3f);
 		Draw.color(Color.lightGray);
 		Lines.rect(x0, y0, w, h);

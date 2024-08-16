d3f68a34e8f53694f5f68416b41c2a8361311bf5
I-hoep1 <78016895+I-hope1@users.noreply.github.com>
2024-08-16 17:50:39 +0800

    :sparkles:使用LetExpr实现switch转换

--- compiler/src/modtools/annotations/unsafe/MyTransPatterns.java
+++ compiler/src/modtools/annotations/unsafe/MyTransPatterns.java
@@ -1,16 +1,15 @@
 package modtools.annotations.unsafe;
 
-import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.code.Symbol.VarSymbol;
+import com.sun.tools.javac.code.*;
 import com.sun.tools.javac.comp.*;
 import com.sun.tools.javac.tree.*;
 import com.sun.tools.javac.tree.JCTree.*;
 import com.sun.tools.javac.util.*;
 
-import java.util.function.Function;
-
 public class MyTransPatterns extends TransPatterns {
-	final SwitchDesugar   switchDesugar;
-	final LambdaToMethod  ltm;
+	final SwitchDesugar  switchDesugar;
+	final LambdaToMethod ltm;
 	public MyTransPatterns(Context context) {
 		super(context);
 		switchDesugar = new SwitchDesugar(context);
@@ -22,51 +21,50 @@ public class MyTransPatterns extends TransPatterns {
 	}
 }
 class SwitchDesugar extends TreeTranslator {
-	final Symtab    syms;
-	final TreeMaker make;
-	final Names     names;
+	final        Symtab    syms;
+	final        TreeMaker make;
+	final        Names     names;
+	public final Check     chk;
 
 	SwitchDesugar(Context context) {
 		syms = Symtab.instance(context);
 		make = TreeMaker.instance(context);
 		names = Names.instance(context);
-	}
-
-	Function<JCExpression, JCStatement> func;
-	@Override
-	public void visitReturn(JCReturn tree) {
-		if (tree.expr instanceof JCSwitchExpression expression &&
-		    expression.cases.stream().anyMatch(c -> c.labels.stream().anyMatch(l -> l instanceof JCPatternCaseLabel))) {
-			func = make::Return;
-			visitSwitchExpression(expression);
-			func = null;
-			return;
-		}
-		super.visitReturn(tree);
-	}
-	public void visitVarDef(JCVariableDecl tree) {
-		if (tree.init instanceof JCSwitchExpression expression &&
-		    expression.cases.stream().anyMatch(c -> c.labels.stream().anyMatch(l -> l instanceof JCPatternCaseLabel))) {
-			tree.init = null;
-			func = v -> make.Exec(make.Assign(tree.vartype, v));
-			visitSwitchExpression(expression);
-			func = null;
-			return;
-		}
-		super.visitVarDef(tree);
+		chk = Check.instance(context);
 	}
 
 	public void visitSwitchExpression(JCSwitchExpression tree) {
-		if (transSwitch(tree.cases, tree.selector)) return;
+		if (transSwitch(tree.cases, tree.selector, tree.type)) return;
 		super.visitSwitchExpression(tree);
 	}
 	@Override
 	public void visitSwitch(JCSwitch tree) {
-		if (transSwitch(tree.cases, tree.selector)) return;
+		if (transSwitch(tree.cases, tree.selector, null)) return;
 		super.visitSwitch(tree);
 	}
-	boolean transSwitch(List<JCCase> cases, JCExpression selector) {
+	class YieldTranslator extends TreeTranslator {
+		int i = 0;
+		String getName() {
+			return "$switch" + i++;
+		}
+		JCVariableDecl variable;
+		public void init(Type exprType) {
+			variable = make.VarDef(new VarSymbol(0, names.fromString(getName()), exprType, exprType.tsym.owner), null);
+		}
+		public <T extends JCTree> T translate(T tree) {
+			if (tree instanceof JCYield yield) {
+				return (T) make.Exec(make.Assign(make.Ident(variable), yield.value).setType(variable.type));
+			}
+			return super.translate(tree);
+		}
+	}
+	final YieldTranslator yieldTranslator = new YieldTranslator();
+	boolean transSwitch(List<JCCase> cases, JCExpression selector, Type exprType) {
 		if (cases.stream().anyMatch(c -> c.labels.stream().anyMatch(l -> l instanceof JCPatternCaseLabel))) {
+			if (exprType != null) {
+				yieldTranslator.init(exprType);
+				yieldTranslator.translate(cases);
+			}
 			JCIf        first  = null;
 			JCStatement lastIf = null;
 			for (JCCase jcCase : cases) {
@@ -77,7 +75,15 @@ class SwitchDesugar extends TreeTranslator {
 			}
 
 			result = first;
-			// println(result);
+			if (exprType != null) {
+				LetExpr expr = make.LetExpr(List.of(
+				 yieldTranslator.variable,
+				 first
+				), make.Ident(yieldTranslator.variable));
+				expr.type = exprType;
+				expr.needsCond = true;
+				result = expr;
+			}
 			return true;
 		}
 		return false;
@@ -85,9 +91,7 @@ class SwitchDesugar extends TreeTranslator {
 
 	JCStatement translateCase(JCExpression selector, JCCase jcCase) {
 		JCExpression ifCondition = null;
-		if (func != null) {
-			translateStats(jcCase, func);
-		}
+
 		make.at(jcCase);
 		JCStatement truepart = jcCase.body instanceof JCBlock b ? b : make.Block(0, jcCase.stats);
 		for (JCCaseLabel label : jcCase.labels) {
@@ -106,19 +110,6 @@ class SwitchDesugar extends TreeTranslator {
 		}
 
 		return ifCondition == null ? truepart : make.If(ifCondition, truepart, null);
-	}
-
-	void translateStats(JCCase jcCase, Function<JCExpression, JCStatement> func) {
-		List<JCStatement> stats = jcCase.stats;
-		TreeTranslator translator = new TreeTranslator() {
-			public <T extends JCTree> T translate(T tree) {
-				if (tree instanceof JCYield yield) return (T) func.apply(yield.value);
-				return super.translate(tree);
-			}
-		};
-		translator.translate(stats);
-		// return make.Apply(List.nil(), make.Select(make.TypeCast(make.Ident(names.fromString("Supplier")),
-		//  make.Lambda(List.nil(), make.Block(0, stats))), names.fromString("get")), List.nil());
 	}
 
 	JCExpression makePatternMatchCondition(JCExpression selector, JCPattern pattern) {
